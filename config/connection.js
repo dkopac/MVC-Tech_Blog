8de7283f8eeb8421e1157d8214a8a0869f1cd176
[Dan <123705636+dkopac@users.noreply.github.com>
2023-05-29 10:46:32 -0400

    changes

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,16 @@
+// Import Sequelize from library
 const Sequelize = require("sequelize");
+
 require("dotenv").config();
 
+// Create connection to database
 let sequelize;
 
+// If deployed, use JAWSDB
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
+
+  // Otherwise, use local database
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
